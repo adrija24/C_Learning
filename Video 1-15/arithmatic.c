1c07319881540996b8af3161f21352dea3cc4eb6
adrija24 <24adrijaroy@gmail.com>
2021-05-27 20:24:19 +0530

    arithmatic operator in C programming added

---
+++ Video 1-15/arithmatic.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    /* code */
+   int a, b;
+   a= 34;
+   b= 6;
+
+   printf("a + b =%d", a+b);
+   printf("a - b =%d", a-b);
+   printf("a * b =%d", a*b);
+   printf("a / b =%d", a/b);
+   
+    return 0;
+}
