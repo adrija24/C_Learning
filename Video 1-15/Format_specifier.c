29869972436a862fb8e74e7e0873a844f64c1af5
adrija24 <24adrijaroy@gmail.com>
2021-05-30 18:41:48 +0530

    To change the number of digits before and after '.' in C programming added

--- Video 1-15/Format_specifier.c
+++ Video 1-15/Format_specifier.c
@@ -3,9 +3,23 @@
 int main(int argc, char const *argv[])
 {
     int a = 7;
-    float b = 3.55;
+    float b = 3.555;
     
     printf("hello world\n");
-    printf("The value of a is %d\nThe value of b is %f", a ,b);
+    printf("The value of a is %d\nThe value of b is %f\n", a ,b);
+    
+    //To change the total number of digits before and after '.':
+    printf("%.4f\n", b );
+    printf("%0.4f\n", b );
+    printf("%10.4f\n", b );
+    printf("%2.4f\n", b );
+    printf("%6.4f\n", b );
+    printf("%8.4f\n", b );
+    printf("%12.4f space\n", b );
+    printf("%-12.4f space\n", b );
+
+
+
+
     return 0;
 }
