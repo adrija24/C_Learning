0c803d7c9323af4ae65d1d69669872f3ee032067
adrija24 <24adrijaroy@gmail.com>
2021-05-30 18:02:11 +0530

    Diference between Intiger and Float learn in C programming added

---
+++ Video 1-15/Format_specifier.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+    int a = 7;
+    float b = 3.55;
+    
+    printf("hello world\n");
+    printf("The value of a is %d\nThe value of b is %f", a ,b);
+    return 0;
+}
