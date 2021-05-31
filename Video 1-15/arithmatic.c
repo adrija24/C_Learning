1b94b2128454271d61f6b7829a3e88545b4156ef
adrija24 <24adrijaroy@gmail.com>
2021-05-31 19:26:53 +0530

    Operaors learning in C programming added

--- Video 1-15/arithmatic.c
+++ Video 1-15/arithmatic.c
@@ -7,12 +7,13 @@ int main()
    a= 34;
    b= 6;
 
+   //Arithmatic operators by %d........
    printf("a + b =%d\n", a+b);
    printf("a - b =%d\n", a-b);
    printf("a * b =%d\n", a*b);
    printf("a / b =%d\n", a/b);
 
-
+   //Arithmatic operators by %f.....
    int p;
    float r;
    p = 34;
@@ -23,24 +24,27 @@ int main()
    printf("p * r = %f\n", p*r);
    printf("p / r = %f\n", p/r);
 
+   //Relational operator.....
    int m, n;
    m = 34;
    n = 34;
     printf("m == n = %d\n", m==n);
 
 
-
+   //Logical AND Operator.......
    int g, f;
    g = 45;
    f = 6;
     printf("g && f = %d\n", g&&f);
 
+    //Logical OR Operator.......
     int s, t;
     s = 46;
     t = 34;
     
     printf("s || t = %d\n", s||t);
 
+    //Logical NOT Operator.......
     int j, k;
     j = 40;
     k = 34;
