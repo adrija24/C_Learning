25b1b45b6bfa73db3273d2dd1118e3829884fa84
adrija24 <24adrijaroy@gmail.com>
2021-06-01 23:42:03 +0530

    If Statement learning in C programming added

---
+++ Video 1-15/If_else_Satement.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main()
+{
+    int age;
+    printf("Enter your age\n");
+    scanf("%d", &age);
+
+    printf("You have entered %d as your age\n", age);
+    if (age > 18)
+    {
+        printf("You can vote!");
+    }
+    
+    
+    
+    
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
