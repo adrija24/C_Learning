a45d0e5c4307a822e500c4d00e38faba2baf1d39
adrija24 <24adrijaroy@gmail.com>
2021-06-13 19:11:59 +0530

    Type casting learning in C Programming

---
+++ Video 1-15/Type_casting.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main()
+{
+    // Type casting....
+    printf("We are going to start type casting\n");
+    int a = 4;
+    float b = 54.4;
+    printf("The number is %d\n", (int) b);
+
+    //Type conversion....
+    float p = 54/4;
+    printf("The result is %f\n", p);
+
+    float r = (float)55/4;
+    printf("The result is %f\n", r);
+    return 0;
+}
+ 
