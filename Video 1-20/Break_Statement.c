915369b11fd6965319a31b87b572ee621bdf65fb
Aritra Roy <aritraroy24@gmail.com>
2022-10-06 08:50:32 +0530

    age changed from 10 to 18

--- Video 1-20/Break_Statement.c
+++ Video 1-20/Break_Statement.c
@@ -3,12 +3,12 @@
 int main(int argc, char const *argv[])
 {
     int i, age;
-    for (i = 0; i < 10; i ++)
+    for (i = 0; i < 18; i ++)
     {
         printf("%d\n", i);
         printf("Enter your age\n");
         scanf("%d", &age);
-        if (age > 10)
+        if (age > 18)
         {
             break;
         }
